15b611ff765e82c4051ef2fe8b18444aac881aef
Allanaire Tapion <allan.t@remotestaff.com.au>
2016-04-22 13:11:04 +0800

    Updates on UserController

---
+++ app/InventoryItem.php
@@ -0,0 +1,14 @@
+<?php
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class InventoryItem extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'inventory_items';
+}
