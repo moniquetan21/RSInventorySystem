f2ed8a7bef6c0eda6d4eadfe446bbeb4a17046f7
Allanaire Tapion <allan.t@remotestaff.com.au>
2016-04-27 17:16:25 +0800

    TEST

--- resources/views/tickets/welcome.blade.php
+++ resources/views/tickets/welcome.blade.php
@@ -10,7 +10,7 @@
 @section('content')
 	<div class="row">
 		<div class="col-lg-12">
-			Welcome {{$client->client_profile()->first_name}} {{$client->client_profile()->first_name}}
+			Welcome {{$client->client_profile->first_name}} {{$client->client_profile->first_name}}
 		</div>
 	</div>
 	<div class="row">
