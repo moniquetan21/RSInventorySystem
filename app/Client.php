31341cf214c109b7709419af788d7ac27a1fba08
Allanaire Tapion <allan.t@remotestaff.com.au>
2016-04-27 17:07:28 +0800

    Test Connect on MySQL

---
+++ app/Client.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model
+{
+	
+	protected $table = "clients";
+	
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 
+    ];
+	
+	public function client_profile(){
+		return $this->hasOne("App\ClientProfile");
+	}
+}
