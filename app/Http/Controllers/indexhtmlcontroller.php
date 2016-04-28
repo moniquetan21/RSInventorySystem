9a52665a8cfe8522f88fd197f8558ff60be2d72a
atkingjeyyms <jamesmonreal.jm@gmail.com>
2016-04-28 13:58:30 +0800

    connecting dashboard
    asdasdasd

---
+++ app/Http/Controllers/indexhtmlcontroller.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class indexhtmlcontroller extends Controller{
+	/**
+	 * Show the login page for the given user
+	 * 
+	 */
+	 public function showIndex(){
+	 	return view("index");
+	 }
+	 
+	/**
+	 * Show the dashboard page for the given user
+	 * 
+	 */
+	 
+	  
+}
