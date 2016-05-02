61a31c22031e2860f7eaef95426c145e1cc474ac
lphilipc <cluisphilip@yahoo.com>
2016-05-02 09:35:14 +0800

    Sign up CSS/JS

--- resources/views/tickets/login.blade.php
+++ resources/views/tickets/login.blade.php
@@ -5,9 +5,9 @@
 
 <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <link href="/css/bootstrap.min.css" rel="stylesheet">
+  <script src="/js/jquery-2.1.1.js"></script>
+  <script src="/js/bootstrap.min.js"></script>
 <style>
  
 </style>
